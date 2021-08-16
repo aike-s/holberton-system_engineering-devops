f6fe9bdefde516ebd680e90db497052fe7abadc1
aike-s <sagrilo804@gmail.com>
2021-08-15 20:58:05 -0500

    [fix] path for update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,10 @@
 # configured to refuse to authenticate using a password
 
 file_line {'Conf to use private key':
-    path => '~/.ssh/config',
+    path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/holberton',
 }
 file_line {'Conf to authenticate using a password':
-    path => '~/.ssh/config'
+    path => '/etc/ssh/ssh_config'
     line => 'PasswordAuthentication no',
 }
