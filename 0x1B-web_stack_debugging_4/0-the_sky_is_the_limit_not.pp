101d83917b4c973721f4b6a2c46547b49e7b8dfd
aike-s <sagrilo804@gmail.com>
2021-10-07 23:10:54 -0500

    [update]

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix problem to handle multiple request at the same time
-exec {'expand-limit':
+exec {'more-requests':
   path    => '/usr/local/bin/:/bin/',
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
 }
